f2f61fa25d84e0a0f4b0d274c5ea9217cb37a819
Manoj Reddy <guttikondamanoj@gmail.com>
2021-08-20 21:55:36 +0530

    Initial Commit

---
+++ iTunes Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTunes Search
+//
+//  Created by MANOJ REDDY on 20/08/21.
+//  Copyright © 2021 MANOJ REDDY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
